2d209f02ea525bc73151b23301fbaab27ed360cc
maniahuv <diennam2000@gmail.com>
2024-11-25 20:28:26 +0700

    change some new

--- AimsProject/StoreTest.java
+++
@@ -1,8 +0,0 @@
-public class StoreTest {
-    public static void main(String[] args) {
-        Store anAvai = new Store();
-        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The lion king", "Animation", "Roger Allers", 87, 19.95f);
-        anAvai.addDvd(dvd1);
-        anAvai.removeDvd(dvd1);
-    }
-}
