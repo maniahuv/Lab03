4906a910672b5272fcf9049884d89e35ec662983
maniahuv <diennam2000@gmail.com>
2024-11-26 00:45:31 +0700

    sua loi

---
+++ OtherProject/Lab01/b63.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class b63{
+    public static void main(String[] arg){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Nhap n: ");
+        int n=scanner.nextInt();
+        int m=2*n-1;
+        for (int y=0;y<n;y++){
+            for (int x=0;x<m;x++){
+                if (x<=n-1 && (y+x-(n-1))>=0 ){
+                    System.out.print("*");
+                } else if (x>=n-1 && (y-x+n-1)>=0){
+                    System.out.print("*");
+                } else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("\n");
+        }
+        scanner.close();
+    }
+}
